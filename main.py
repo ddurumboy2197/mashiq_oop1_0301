6ae96ff86c6934754a17f3cf9c6ac6496c5220cf
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-01-03 13:42:00 +0500

    Add product management functionality to main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print()
+from  dokon import Dokon
+from  maxsulot import Mahsulot
+
+product1 = Mahsulot('Olma', 20000, 100)
+product2 = Mahsulot('Anjir', 23000, 30)
+product3 = Mahsulot('Olma', 37000, 50)
+
+market = Dokon()
+market.mahsulot_qoshish(product1)
+market.mahsulot_qoshish(product2)
+market.mahsulot_qoshish(product3)
+market.maxsulotlar_royxati()
+
+market.umumiy_qiymat()
